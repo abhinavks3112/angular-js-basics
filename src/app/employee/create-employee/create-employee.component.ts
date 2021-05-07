4b58758ea540df3c346791330a0812d46af207b1
Abhinav <abhiks3112@gmail.com>
2021-05-07 23:54:41 +0530

    Forms validation added, setValue and patchValue's uses shown

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,24 +8,29 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  employeeForm = new FormGroup({
+  employeeForm = this.formBuilder.group({
     fullName: new FormControl(),
     email: new FormControl()
   });
 
+   /*
+   FormBuilder class provides a syntactic sugar or another way of declaring the formgroup, forcontrol and array instance.
+   It must be injected into the class's constructor first to use it elsewere on the form
+  */
   constructor(private formBuilder: FormBuilder) {
     this.createForm();
   }
 
  createForm(){
+ 
   this.employeeForm = this.formBuilder.group({
-    fullName: '',
-    email: '',
+    fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+    email: [''],
     // Creating nested formgroup skills
-    skills: new FormGroup({
-      skillName: new FormControl(),
-      proficiency: new FormControl(),
-      experienceInYears: new FormControl()
+    skills: this.formBuilder.group({
+      skillName: [''],
+      proficiency: ['beginner'],
+      experienceInYears: ['']
     })
   });
  }
@@ -34,11 +39,39 @@ export class CreateEmployeeComponent implements OnInit {
    
   }
 
+  onLoadDataClick(): void
+  {
+    /*
+     * when setValue is used then all control values MUST be given, we cannot leave any property out(eg. not giving value for skills will give error) 
+     * when patchValue is used then we can update a subset of form controls Or we CAN update all values also
+    */
+    // this.employeeForm.setValue({
+    //   fullName: "Jack",
+    //   email: "Jack@abcdomain.com"
+    //   /* skills: {
+    //     skillName: "C#",
+    //     proficiency: "advanced",
+    //     experienceInYears: "6"
+    //   } */
+    // })
+
+    this.employeeForm.patchValue({
+      fullName: "Jack",
+      email: "Jack@abcdomain.com",
+      skills: {
+        skillName: "C#",
+        proficiency: "advanced",
+        experienceInYears: "6"
+      } 
+    })
+  }
+
   onSubmit(): void {
     console.log(this.employeeForm.touched);
     console.log(this.employeeForm.value);
 
     console.log(this.employeeForm.controls.fullName.touched);
+    console.log(this.employeeForm.controls.fullName.errors);
   }
 
 }
